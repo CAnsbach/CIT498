db00d15426905a0f9b999297ad7ab2073bccf905
Christopher Ansbach <cansbach1998@gmail.com>
2021-09-19 13:22:33 -0400

    Added Code and Activity
    Added a login screen (does not have functionality yet) and added code to MainActivity.java to show a toast notification when the login button is pressed.

--- CampusEventTracker/app/src/main/java/com/example/campuseventtracker/MainActivity.java
+++ CampusEventTracker/app/src/main/java/com/example/campuseventtracker/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.campuseventtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity
+{
+    EditText inpUsername, inpPassword;
+    Button btnLogin;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inpUsername =  findViewById(R.id.txtUsernameInput);
+        inpPassword = findViewById(R.id.txtPasswordInput);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), inpUsername.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
